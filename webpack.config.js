2a24310ed2120bc6b9b0fa25d9e3bd30e077a164
Tanya <an5227952@gmail.com>
2025-04-11 14:29:16 +0300

    changed dirname

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             },
     devServer: {
         static: {
-        directory: path.join(__dirname, './public'),
+        directory: path.join(__dirname, './src/js'),
               },
         compress: true,
         port: 9000,
